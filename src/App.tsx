7cff2ee54220ec28960de0a0f75b66a189239a58
Jonathan Chin <jonathankchin@gmail.com>
2024-08-31 10:11:18 -0400

    feat: add basic styling to table | clean up styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useState, useEffect, Suspense } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { CollisionData } from './core/interface/collision-data.interface'
 import './App.css'
 import Table from './components/table'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [data, setData] = useState<CollisionData[]>([])
 
   useEffect(() => {
@@ -22,28 +19,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Suspense>
+          <div className="table-container">
+            <Table tableData={data}></Table>
+          </div>
+        </Suspense>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Suspense>
-        <Table tableData={data}></Table>
-      </Suspense>
     </>
   )
 }
