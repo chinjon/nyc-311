abd20298d8675f008cad2dd229aca01fc6fb36ec
Jonathan Chin <jonathankchin@gmail.com>
2024-09-06 18:31:26 -0400

    feat: update type

--- src/core/interface/collision-data.interface.ts
+++ src/core/interface/collision-data.interface.ts
@@ -3,6 +3,8 @@ export type CollisionData = {
   readonly contributing_factor_vehicle_1: string;
   readonly contributing_factor_vehicle_2: string;
   readonly crash_date: string;
+  readonly longitude?: string;
+  readonly latitude?: string;
   readonly create_date: string;
   readonly number_of_cyclist_injured?: string;
   readonly number_of_cyclist_killed?: string;
@@ -12,6 +14,6 @@ export type CollisionData = {
   readonly number_of_persons_injured?: string;
   readonly off_street_name?: string;
   readonly on_street_name?: string;
-  readonly vehicle_type_code1: string;
-  readonly vehicle_type_code_2: string;
+  readonly vehicle_type_code_1?: string;
+  readonly vehicle_type_code_2?: string;
 }
